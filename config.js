4c727451f17afad5c9fd29ed74929c942a03f08e
Abdudochex <beout.support@gmail.com>
2026-07-16 01:13:59 +0100

    config.js
    Updated bot configuration details including pairing number, bot name, author, and source.

--- config.js
+++ config.js
@@ -1,22 +1,24 @@
 import { watchFile, unwatchFile } from 'fs';
 import chalk from 'chalk';
 import { fileURLToPath } from 'url';
-import { Button, ButtonV2,Carousel, AIRich } from './lib/MessageBuilder.js';
+import { Button, ButtonV2, Carousel, AIRich } from './lib/MessageBuilder.js';
 
 global.Button = Button;
 global.ButtonV2 = ButtonV2;
 global.Carousel = Carousel;
 global.AIRich = AIRich;
 
-global.pairingNumber = 212666666666;
+// ضع هنا الرقم المخصص لربط البوت (Pairing Code) إذا كنت تستخدمه
+global.pairingNumber = 212621790049; 
+
 global.owner = [
-  ['212717457920', 'noureddine Ouafy', true],
-  ['', 'Owner 2', true],
+  ['212610591087', 'ABDENNACER CHAKIR', true],
+  ['212621790049', 'ARFOXBOT STREAM', true], // تم إدراج اسمك ورقمك هنا كمالك أساسي للبوت
 ];
 
-global.namebot = 'gaff ai';
-global.author = 'Noureddine ouafy';
-global.source = 'https://chat.whatsapp.com/Hp3R0WWD5G8Li9HKBU6fn3';
+global.namebot = 'ARFOXBOT STREAM'; // تم تعديل اسم البوت هنا
+global.author = 'ABDENNACER CHAKIR'; // تم تأكيد اسمك هنا
+global.source = 'https://www.instagram.com/abden__nacer?igsh=MXZpNWRhczFicGU0Mg==';
 
 global.wait = 'Loading... | جاري الانتظار';
 global.eror = 'There is an error... | وقع خطأ';
@@ -28,7 +30,7 @@ global.pakasir = {
 };
 
 global.stickpack = 'Created By';
-global.stickauth = namebot;
+global.stickauth = global.namebot; // سيأخذ اسم البوت تلقائياً لحقوق الملصقات
 
 global.multiplier = 38; // The higher, The harder levelup
 
